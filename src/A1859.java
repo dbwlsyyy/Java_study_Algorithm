b5207a126566c20995ce739d5ae26f0b9c824571
홍유진 <dbwlsyyy@naver.com>
2024-02-11 18:05:07 +0900

    5th commit

--- src/A1859.java
+++ src/A1859.java
@@ -1,4 +1,6 @@
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.io.FileInputStream;
 
@@ -6,7 +8,7 @@ import java.io.FileInputStream;
    사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
    이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
  */
-class A18799 // 평균의 평균
+class A1859 // 백만장길
 {
     public static void main(String args[]) throws Exception
     {
@@ -23,47 +25,70 @@ class A18799 // 평균의 평균
 		   표준입력 System.in 으로부터 스캐너를 만들어 데이터를 읽어옵니다.
 		 */
         Scanner sc = new Scanner(System.in);
+        List<Integer> result = new ArrayList<Integer>();
+
         int T;
         T=sc.nextInt();
 		/*
 		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 		*/
 
-        for(int test_case = 1; test_case <= T; test_case++) {
-
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            int have = 0;
+            int _have = 0;
+            int money = 0;
+            int maxDay = -1;
+            int max = 0;
             int n = sc.nextInt();
-
-            String num = "";
+            int N = n;
+            int[] price = new int[n];
             for(int i = 0; i < n; i++){
-                num += sc.next();
+                price[i] = sc.nextInt();
             }
-            StringBuilder sb = new StringBuilder(num.trim());
-            StringBuilder _sb = new StringBuilder(sb);
 
-            for (int j = 0; j < n; j++) {
-                for (int k = j + 1; k <= n; k++) {
-                    if (k <= sb.length()) {
-                        String subNum = sb.substring(j, k);
-                        System.out.println(subNum);
+            while(N > 0) {
+
+                for (int i = maxDay + 1; i < n; i++) {
+                    if (price[i] >= max) {
+                        max = price[i];
+                        maxDay = i;
                     }
                 }
 
-                while (j < _sb.length() - 1) {
-                    _sb.deleteCharAt(j + 1); // 중간 문자 삭제
-                    for (int k = j + 1; k <= n; k++) { // 시작 인덱스 수정
-                        if (k <= _sb.length()) {
-                            String _subNum = _sb.substring(j, k); // 중간 문자가 삭제된 _sb 사용
-                            if(_subNum.length() != 1){
-                                System.out.println(_subNum);
-                            }
-                        }
-                    }
+                if (maxDay == 0) {
+                    money = 0;
+                    N = (price.length-1) - maxDay;
+                    max = price[maxDay+1];
+                } else {
+                    for (int k = _have; k < maxDay; k++) {
+                        have++;
 
+                        if(price[k] > price[k+1]) k++;
+                        money -= price[k];
+                    }
+                    money += have * price[maxDay];
+                    _have += have;
+                    have = 0;
+                    N = (price.length-1) - maxDay;
+                    if(maxDay == n-1) {
+                        max = price[maxDay];
+                    }else max = price[maxDay+1];
                 }
+                if(money < 0) money = 0;
+
+                if(N == 0) result.add(money);
+
             }
 
-            //System.out.println("#" + test_case + " " + count);
+
+
+
+
+
         }
-
+        for(int i = 1; i <= T; i++){
+            System.out.println("#" + i + " " + result.get(i-1));
+        }
     }
 }
